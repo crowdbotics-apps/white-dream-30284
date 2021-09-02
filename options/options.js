a2a90359cfb8fbca7a315e75384e83e64b2eaa37
Crowdbotics <team@crowdbotics.com>
2021-09-02 19:55:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "white_dream_30284Identifier",
+  url: "https://white_dream_30284Identifier.botics.co",
+  api: "https://white_dream_30284Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
